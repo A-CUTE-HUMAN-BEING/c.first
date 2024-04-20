8ace33e1d6c29f0d454c91488f8e2f7e44ec000f
C-C-C-cat <fccckheal@gmail.com>
2024-04-20 12:24:46 +0800

    链表

---
+++ 链表/链表/test.c
@@ -0,0 +1,36 @@
+#include"SQlist.h"
+void test()
+{
+	int i = 0;
+	int num = 0;
+	SLnode* sqlist = NULL;
+	SLnode* pos = NULL;
+	SLnode* pos1 = NULL;
+	SQpushFront(&sqlist, 200);
+	PrintSQ(sqlist);
+	SQpopFront(&sqlist);
+	PrintSQ(sqlist);
+	SQpushBack(&sqlist, 100);
+	PrintSQ(sqlist);
+	SQpopBack(&sqlist);
+	PrintSQ(sqlist);
+	SQpushFront(&sqlist, 1);
+	SQpushFront(&sqlist, 2);
+	SQpushFront(&sqlist, 3);
+	SQpushBack(&sqlist, 4);
+	SQpushBack(&sqlist, 5);
+	SQpushBack(&sqlist, 6);
+	SQpushBack(&sqlist, 7);
+	PrintSQ(sqlist);
+	pos = SQfind(sqlist, 4);
+	SQinsertPosAfter(pos, 10000);
+	PrintSQ(sqlist);
+	pos1 = SQfind(sqlist, 4);
+	SQerasePosAfter(pos1);
+	PrintSQ(sqlist);
+}
+int main()
+{
+	test();
+	return 0;
+}
